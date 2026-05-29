044b6d8731f31c8389c4a8da4e0a3a18da9ae39f
senghong <senghong.learning@gmail.com>
2026-05-29 22:12:31 +0700

    Add env

--- core-set/src/api/axios.ts
+++ core-set/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://core.shkh1601.workers.dev/api",
+  baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
